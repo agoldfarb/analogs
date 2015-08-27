4e64edbc11d86e029070336ccae62766d7b37ba8
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-08-27 12:36:35 -0500

    vinyl controller

---
+++ app/controllers/vinyls_controller.rb
@@ -0,0 +1,39 @@
+class VinylsController < ApplicationController
+  def index
+    @vinyls = Vinyl.all
+  end
+
+  def show
+    @vinyl = Vinyl.find(params[:id])
+  end
+
+  def new
+    @vinyl = Vinyl.new
+  end
+
+  def create
+    @vinyl = Vinyl.new(params.require(:vinyl).permit(:title, :artist, :year, :label))
+     if @vinyl.save
+       flash[:notice] = "Album was saved."
+       redirect_to @vinyl
+     else
+       flash[:error] = "There was an error saving the album. Please try again."
+       render :new
+     end
+  end
+
+  def edit
+    @vinyl = Vinyl.find(params[:id])
+  end
+
+  def update
+    @vinyl = Vinyl.find(params[:id])
+     if @vinyl.update_attributes(params.require(:vinyl).permit(:title, :artist, :year, :label))
+       flash[:notice] = "Album was updated."
+       redirect_to @vinyl
+     else
+       flash[:error] = "There was an error saving the album. Please try again."
+       render :edit
+     end
+  end
+end
